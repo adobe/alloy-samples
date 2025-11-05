841ff61c80337a109b9eb8f9e2364f36a251cf24
Carter McBride <cmcbride@adobe.com>
2025-11-05 14:28:31 -0700

    Add server routes to log

--- chatgpt-app/backend/index.js
+++ chatgpt-app/backend/index.js
@@ -2,6 +2,8 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { serve } from "@hono/node-server";
 import { StreamableHTTPTransport } from "@hono/mcp";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+
 import process from "node:process";
 
 const app = new Hono();
@@ -10,6 +12,10 @@ const mcpServer = new McpServer({
   name: "alloy-vacations",
   version: "1.0.0",
 });
+
+const LOG_PREFIX = "[alloy-vacations-backend] ";
+const log = (...args) => console.log(LOG_PREFIX, ...args);
+app.use(logger(log));
 
 app.get("/", (c) => {
   return c.text("Hello, World!");
@@ -25,8 +31,12 @@ const server = serve(app, (addressInfo) => {
   const address = `http://${
     addressInfo.address === "::" ? "[::1]" : addressInfo.address
   }:${addressInfo.port}`;
-  console.log("[alloy-vacations-backend] listening on", address);
+  log("[alloy-vacations-backend] listening on", address);
 });
+log("[alloy-vacations-backend] valid endpoints are:");
+for (const route of app.routes) {
+  log(`  - [${route.method}] ${route.path}`);
+}
 
 process.on("SIGINT", () => {
   server.close();
